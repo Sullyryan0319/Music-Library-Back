0798351e9f252327908904c9e2ae7c67808f8ada
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-10 19:15:27 -0700

    Test 'post song' method + upload screenshot for grading

--- index.js
+++ index.js
@@ -15,10 +15,17 @@ app.get('/api/songs', (req, res) => {
     const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
    });
-   
 
-   app.get('/api/songs/:id', (req, res) => {
+
+app.get('/api/songs/:id', (req, res) => {
     const id = req.params.id;
     const song = repoContext.songs.findSongById(id);
     return res.send(song);
+   });
+
+
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
    });
